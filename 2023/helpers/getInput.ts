483c1276756227dcdf4344b24f3c054518fe06af
Bradley Taylor <qzi@qlik.com>
2024-12-01 20:14:11 -0800

    2024 initial commit

--- 2023/helpers/getInput.ts
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-export const getInput = (inputPath: string) => {
-  return fs.readFileSync(inputPath, 'utf8').trim().split('\n');
-};
---
+++ 2024/day01/main_test.go
@@ -0,0 +1,5 @@
+package main
+
+func testPart1() {}
+
+func testPart2() {}
