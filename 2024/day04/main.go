d1a2227df79d1f3528e23dacc9cfa0bc35f80c2e
Bradley Taylor <qzi@qlik.com>
2024-12-06 12:25:56 -0800

    day 4 part 1

---
+++ 2024/day04/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	_ "embed"
+	"strings"
+)
+
+//go:embed day4-input.txt
+var input string
+
+//go:embed day4-example.txt
+var exampleInput string
+
+func init() {
+	input = strings.TrimRight(input, "\n")
+	exampleInput = strings.TrimRight(exampleInput, "\n")
+}
+
+const XMAS = "XMAS"
+
+var directions = map[string][2]int{
+	"up":         {-1, 0},
+	"down":       {1, 0},
+	"left":       {0, -1},
+	"right":      {0, 1},
+	"up left":    {-1, -1},
+	"up right":   {-1, 1},
+	"down left":  {1, -1},
+	"down right": {1, 1},
+}
+
+func Part1(input string) int {
+	matrix := formatInput(input)
+	var result int
+
+	for i, row := range matrix {
+		for j := range row {
+			if matrix[i][j] == string(XMAS[0]) {
+				for d, dir := range directions {
+					row, col := i+dir[0], j+dir[1]
+					if dfs(matrix, row, col, 1, d) {
+						result += 1
+					}
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func Part2(input string) int {
+	// matrix := formatInput(input)
+	return 0
+}
+
+func formatInput(input string) [][]string {
+	rows := strings.Split(input, "\n")
+	var result [][]string
+	for _, s := range rows {
+		var level []string
+		for _, ch := range s {
+			level = append(level, string(ch))
+		}
+		result = append(result, level)
+	}
+	return result
+}
+
+func dfs(matrix [][]string, row, col, curr int, dir string) bool {
+	if curr == len(XMAS) {
+		return true
+	}
+
+	if row < 0 || row >= len(matrix) || col < 0 || col >= len(matrix[0]) {
+		return false
+	}
+
+	if matrix[row][col] != string(XMAS[curr]) {
+		return false
+	}
+
+	r, c := row+directions[dir][0], col+directions[dir][1]
+
+	return dfs(matrix, r, c, curr+1, dir)
+}
---
+++ 2024/day04/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	tests := []struct {
+		name, input string
+		expected    int
+	}{
+		{"Day 4 Example input", exampleInput, 18},
+		{"Day 4 Actual input", input, 166630675},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Part1(tt.input)
+
+			if got != tt.expected {
+				t.Errorf("Answer was incorrect. Got: %d, Expected: %d", got, tt.expected)
+			}
+		})
+	}
+}
+
+// func TestPart2(t *testing.T) {
+// 	tests := []struct {
+// 		name, input string
+// 		expected    int
+// 	}{
+// 		{"Day 4 Example input", exampleInput, 48},
+// 		{"Day 4 Actual input", input, 93465710},
+// 	}
+//
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got := Part2(tt.input)
+//
+// 			if got != tt.expected {
+// 				t.Errorf("Answer was incorrect. Got: %d, Expected: %d", got, tt.expected)
+// 			}
+// 		})
+// 	}
+// }
